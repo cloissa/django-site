1de3a3d722f065c6b4934bb91dff1c6a00a55658
Chloe Umble <chloe@thehappyhome.co>
2017-04-02 16:50:58 -0700

    warming up

--- chloesite/views.py
+++ chloesite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'chloesite/index.html', context)
+
+def detail(request, question_id):
+	return HttpResponse("Question #%s" % question_id)
